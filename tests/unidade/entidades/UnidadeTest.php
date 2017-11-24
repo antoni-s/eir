3d2d78b613a83c5fc18b6e29ab6a6c15ae542aeb
antlisufg <antonio.silva27@outlook.com>
2017-11-24 09:14:50 -0200

    tests - Unidade[Entidades]

--- tests/unidade/entidades/UnidadeTest.php
+++ tests/unidade/entidades/UnidadeTest.php
@@ -6,7 +6,7 @@ use App\Models\Entidades\Unidade;
 //include_once __DIR__.'/../../../App/Models/Entidades/Unidade.php';
 
 /**
- * Testes para a classe Unidades / Entidades.
+ * 
  */
 
 class UnidadeTest extends TestCase
